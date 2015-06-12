d1f8bed8b493466d317de056a7dd9b2a916f1a45
qzo <qzo@users.noreply.github.com>
2015-06-12 13:52:54 +0200

    Added AutoVer package

---
+++ packages/manual/autover/tools/chocolateyinstall.ps1
@@ -0,0 +1,52 @@
+﻿$packageName = 'autover'
+$installerType = 'exe'
+$url = 'http://beanland.net.au/autover/AutoVer-setup.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+
+$serviceName = 'AutoVerService'
+$running = 'Running'
+
+# Stops AutoVer process and service
+Function AutoVerStop {
+    $processName = $packageName
+    Stop-Process -Name $processName -ErrorAction SilentlyContinue
+
+    $serviceStopped = $FALSE
+    $service = Get-Service $serviceName -ErrorAction SilentlyContinue
+    If ($service -and ($service.Status -eq $running)) {
+        try {
+            $service.Stop()
+            $serviceStopped = $TRUE
+        } catch {
+            Write-Warning "The running AutoVer service couldn't be stopped"
+        }
+    }
+
+    Return $serviceStopped
+}
+
+# Starts AutoVer service (the process is started automatically after install)
+Function AutoVerStartService {
+    $Service = Get-Service $serviceName -ErrorAction SilentlyContinue
+    If ($Service -and ($Service.Status -ne $running)) {
+        try {
+            $Service.Start()
+        } catch {
+            Write-Warning "The AutoVer service couldn't be started"
+        }
+    }
+}
+
+# SCRIPT STARTS HERE #
+
+# According to the installer, AutoVer (and its service) must not be running,
+# otherwise a restart is needed after the installation
+$serviceStopped = AutoVerStop
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" `
+    -validExitCodes $validExitCodes
+
+If ($serviceStopped) {
+    AutoVerStartService
+}
---
+++ packages/manual/autover/tools/chocolateyuninstall.ps1
@@ -0,0 +1,28 @@
+﻿$packageName = 'autover'
+$registryUninstallerKeyName = 'AutoVer_is1'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+$validExitCodes = @(0)
+
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$regPathWow6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+
+$actualPath = If (Test-Path $regPath) { $regPath } `
+    ElseIf (Test-Path $regPathWow6432) { $regPathWow6432} Else { $FALSE }
+$uninstallerPath =
+    If ($actualPath) {
+        (Get-ItemProperty -Path $actualPath -ErrorAction SilentlyContinue).UninstallString
+    } Else { $FALSE }
+
+If ($uninstallerPath) {
+    # The uninstaller doesn't stop the application, causing some files
+    # not to be deleted, so we need to stop it manually
+    $processName = $packageName
+    Stop-Process -Name $processName -ErrorAction SilentlyContinue
+
+    Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $uninstallerPath
+}
+Else
+{
+    Write-Warning "The registry uninstall data couldn't be found."
+}
