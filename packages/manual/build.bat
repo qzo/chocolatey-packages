c3213901c1bcb1d650c090aca16dc80714be4829
qzo <qzo@users.noreply.github.com>
2015-06-19 23:27:09 +0200

    Build script doesn't check / remove (Vim) backup / undo / swap files (instead NuSpec filtering is used) Build script can now preprocess PHP files and build multiple packages in one run

--- packages/manual/build.bat
+++ packages/manual/build.bat
@@ -1,10 +1,14 @@
-@REM Run "build 'package'" (f.i. "build autover") to check for Vim's swap files,
-@REM remove Vim's backup and undo files and then build the specified package
+@REM Run "build 'package'" (f.i. "build autover" or
+@REM "build farr\find-and-run-robot") to process PHP files (if there are any)
+@REM and build the specified NuGet package using cpack
+@REM
+@REM More packages can be built using one command if they are put in a directory
+@REM whose name ends in "_m" (f.i. "build farr_m)
 
 @ECHO OFF
 
 IF "%1"=="" (
-    ECHO You must supply a package name
+    ECHO You must supply a directory name
     EXIT /B 1
 )
 
@@ -13,22 +17,30 @@ SET StartDir=%cd%
 
 CD /D %PackageDir%
 IF ERRORLEVEL 1 (
-    ECHO The package directory doesn't exist
+    ECHO The package directory (%1^) doesn't exist, exiting...
     EXIT /B 1
 )
 
-ECHO Checking for Vim swap files... & @ECHO.
-DIR *.sw? /B/S /AH
-IF NOT ERRORLEVEL 1 (
-    ECHO Found Vim swap file(s^) (see above^), exiting...
-    CD /D %StartDir%
-    EXIT /B 1
+ECHO. & ECHO Processing PHP files... & ECHO.
+FOR /F %%a IN ('DIR *.php /B/S ^| FINDSTR /LIE ".php"') DO (
+    CD /D %%~da%%~pa
+    php -d short_open_tag=0 %%~a > %%~na
+    CD /D %PackageDir%
 )
 
-ECHO Deleting Vim undo and backup files... & @ECHO.
-DEL /F/S/Q *~
-
-ECHO Creating Chocolatey package... & @ECHO.
-cpack
+IF "%PackageDir:~-2%"=="_m" (
+    ECHO Processing all packages in the directory %1...
+    FOR /F %%d IN ('DIR /A:D /B') do (
+        CD /D %%d
+        IF EXIST %%d.nuspec (
+            ECHO. & ECHO Creating Chocolatey package (%%d^)... & ECHO.
+            cpack
+        )
+        CD /D %PackageDir%
+    )
+) ELSE (
+    ECHO Creating Chocolatey package (%1^)... & ECHO.
+    cpack
+)
 
 CD /D %StartDir%
