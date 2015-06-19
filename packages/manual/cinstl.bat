a6e7bbd96f9632d868149aa4c861eeb7ad64421a
qzo <qzo@users.noreply.github.com>
2015-06-19 23:29:24 +0200

    Install script now also works with subdirectories

--- packages/manual/cinstl.bat
+++ packages/manual/cinstl.bat
@@ -1,10 +1,12 @@
-@REM Run "cinstl 'package'" (f.i. "cinstl autover") to install a package from
-@REM this directory. It's the same as calling "cinst 'package' -fdv -s "%%cd%%"
+@REM Run "cinstl 'package'" (f.i. "cinstl autover" or
+@REM "cinstl farr_m\find-and-run-robot") to install a package from this
+@REM directory. It's the same as calling "cinst 'package' -fdv -s "%%cd%%""
+@REM from inside the package directory
 
 @ECHO OFF
 
 IF "%1"=="" (
-    ECHO You must supply a package name
+    ECHO You must supply a directory name
     EXIT /B 1
 )
 
@@ -17,8 +19,9 @@ IF ERRORLEVEL 1 (
     ECHO The package directory doesn't exist
     EXIT /B 1
 )
-CD /D %ScriptDir%
 
-cinst %1 -fdv -s "%cd%"
+FOR /F %%i IN ('DIR *.nuspec /B') DO SET Package=%%~ni
+
+cinst %Package% -fdv -s "%cd%"
 
 CD /D %StartDir%
