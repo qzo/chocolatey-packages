6b892168b853ce537cf9f1b25aa2009f5f266a5a
qzo <qzo@users.noreply.github.com>
2015-06-20 15:05:09 +0200

    Find and Run Robot Portable package added

---
+++ packages/manual/farr_m/find-and-run-robot.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'find-and-run-robot.portable'
+$url = 'http://www.donationcoder.com/Software/Mouser/findrun/downloads/FindAndRunRobotPortable.zip'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$processName = 'FindAndRunRobot'
+
+Stop-Process -Name $processName -ErrorAction SilentlyContinue
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
---
+++ packages/manual/farr_m/find-and-run-robot.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'find-and-run-robot.portable'
+$fileName = 'FindAndRunRobotPortable.zip'
+
+$processName = 'FindAndRunRobot'
+
+Stop-Process -Name $processName -ErrorAction SilentlyContinue
+
+UnInstall-ChocolateyZipPackage $packageName $fileName
