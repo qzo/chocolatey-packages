89db5180ef615234c6f45187fd41a16e21a94ed7
qzo <qzo@users.noreply.github.com>
2016-01-04 17:28:10 +0100

    Find and run robot package updated

--- packages/manual/farr_m/find-and-run-robot.install/find-and-run-robot.install.nuspec.php
+++ packages/manual/farr_m/find-and-run-robot.install/find-and-run-robot.install.nuspec.php
@@ -1,4 +1,4 @@
-<?php include '../common/common.inc'; ?>
+<?php require '../common/common.inc'; ?>
 <?xml version="1.0" encoding="utf-8"?>
 <!-- Do not remove this test for UTF-8: if “Ω” doesn’t appear as greek uppercase omega letter enclosed in quotation marks, you should use an editor that supports UTF-8, not this one. -->
 <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
@@ -16,6 +16,10 @@
     <licenseUrl><?php echo LICENSE_URL; ?></licenseUrl>
     <requireLicenseAcceptance>false</requireLicenseAcceptance>
     <iconUrl><?php echo ICON_URL; ?></iconUrl>
+    <releaseNotes><?php echo RELEASE_NOTES; ?></releaseNotes>
+    <packageSourceUrl>https://github.com/qzo/chocolatey-packages/tree/master/packages/manual/farr_m/find-and-run-robot.install</packageSourceUrl>
+    <docsUrl><?php echo DOCS_URL; ?></docsUrl>
+    <mailingListUrl><?php echo MAILING_LIST_URL; ?></mailingListUrl>
   </metadata>
   <files>
     <file src="tools\*.*" target="tools" exclude="**\*.swp;**\*~" />
