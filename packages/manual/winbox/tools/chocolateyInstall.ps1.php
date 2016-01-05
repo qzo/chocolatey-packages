2ff007910e6ad09037939ec4308781b66fc48d5d
qzo <qzo@users.noreply.github.com>
2016-01-05 12:36:16 +0100

    Winbox package added

---
+++ packages/manual/winbox/tools/chocolateyInstall.ps1.php
@@ -0,0 +1,38 @@
+﻿<?php require '../vars.inc'; ?>
+$packageName = 'winbox'
+$appName = 'WinBox'
+$url = '<?php echo URL; ?>'
+$dir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$exe = $packageName + '.exe'
+$fullPath = Join-Path -Path $dir -ChildPath $exe
+$checksum = '<?php echo CHECKSUM; ?>'
+$checksumType = 'sha1'
+
+$startFolder = $appName
+$processName = '<?php echo PROCESS_NAME; ?>'
+
+Function CreateStartMenuShortcut {
+    $startMenuFolderPath = Join-Path -Path ([Environment]::GetFolderPath('Programs')) `
+        -ChildPath $startFolder
+    $startMenuFolder = New-Item $startMenuFolderPath -Type Directory `
+        -ErrorAction SilentlyContinue
+    If ($startMenuFolder) {
+            Install-ChocolateyShortcut -shortcutFilePath `
+                (Join-Path -Path $startMenuFolderPath -ChildPath ($appName + '.lnk')) `
+                -targetPath $fullPath -workingDirectory $dir
+    } Else {
+        Write-Warning "Start menu folder couldn't be created."
+    }
+}
+
+# SCRIPT STARTS HERE #
+
+Stop-Process -Name $processName -ErrorAction SilentlyContinue
+
+Get-ChocolateyWebFile -packageName $packageName -fileFullPath $fullPath -url $url -checksum $checksum -checksumType $checksumType
+
+# create .gui file
+New-Item -Path $dir -Name ($exe + '.gui') -ItemType file
+
+CreateStartMenuShortcut
+
---
+++ packages/manual/winbox/tools/chocolateyUninstall.ps1.php
@@ -0,0 +1,13 @@
+﻿<?php require '../vars.inc'; ?>
+$startFolder = 'WinBox'
+
+Function RemoveStartMenuShortcut {
+    $startMenuFolderPath = Join-Path -Path ([Environment]::GetFolderPath('Programs')) `
+        -ChildPath $startFolder
+    Remove-Item $startMenuFolderPath -Recurse -Force -Confirm:$FALSE -ErrorAction SilentlyContinue
+}
+
+# SCRIPT STARTS HERE #
+
+Stop-Process -Name '<?php echo PROCESS_NAME; ?>' -ErrorAction SilentlyContinue
+RemoveStartMenuShortcut
