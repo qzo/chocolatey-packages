3dbc5d53c0315720313eb96064601992d7489957
qzo <qzo@users.noreply.github.com>
2018-03-17 23:10:22 +0100

    TigerVNC updated to 1.8.0

--- packages/manual/tigervnc_m/tigervnc/tools/chocolateyInstall.ps1
+++ packages/manual/tigervnc_m/tigervnc/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'tigervnc'
 $installerType = 'exe'
-$url32 = 'https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc-1.7.1.exe'
-$url64 = 'https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc64-1.7.1.exe'
-$checksum32 = 'e339062ab3a04dfd37f1442efb34100141f0c13b'
-$checksum64 = '94360e688f7964b684f24c4b5f359e1053957a51'
+$url32 = 'https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc-1.8.0.exe'
+$url64 = 'https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc64-1.8.0.exe'
+$checksum32 = '3b6d87b810647d275082ba023977cb35236ee3b5'
+$checksum64 = 'f6ce938ee99fe5c1ca246628725065f438ef092c'
 $checksumType = 'sha1'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0)
