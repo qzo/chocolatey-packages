fc04fd87f71b6ed2fa43f842153c0ba596eb8d9f
qzo <qzo@users.noreply.github.com>
2020-03-15 20:43:42 +0100

    Find and Run Robot updated to 2.239.01, uses sha512

--- packages/manual/farr_m/find-and-run-robot.install/tools/chocolateyinstall.ps1
+++ packages/manual/farr_m/find-and-run-robot.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $installerType = 'exe'
 $url = 'http://www.donationcoder.com/Software/Mouser/findrun/downloads/FindAndRunRobotSetup.exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0)
-$checksum = '4bf4b7ffdd3ed8fabf791354f93d18aae7570298'
-$checksumType = 'sha1'
+$checksum = '6a71f9218c4c14c0c2e9024b56ec474bbc84e67e9d4e01db1bd6aef226c75d4051f00b4fe55192c9947729d4c8aaa510a441e0c16f06c99a0e8d1300d908e392'
+$checksumType = 'sha512'
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url `
     -validExitCodes $validExitCodes -Checksum $checksum -ChecksumType $checksumType
--- packages/manual/farr_m/find-and-run-robot.portable/tools/chocolateyinstall.ps1
+++ packages/manual/farr_m/find-and-run-robot.portable/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'find-and-run-robot.portable'
 $url = 'http://www.donationcoder.com/Software/Mouser/findrun/downloads/FindAndRunRobotPortable.zip'
-$checksum = '3183e972bf5d22e0051c1c5907aad3dc58206ce4'
-$checksumType = 'sha1'
+$checksum = 'd4cf6a9fae371fb1ed2e17de559e86c14c53f906b3a78123d7e02034c23f91895bb58f215897d5fd744b2feb156855d29fac0935da301f7e99ce8d151c471655'
+$checksumType = 'sha512'
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $processName = 'FindAndRunRobot'
 
